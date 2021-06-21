c6e4752a05b08ccb3181cd19b10f63719b8dc8b4
ac-james <annjames.email@gmail.com>
2021-06-21 11:21:00 +0100

    Add javascript source
    add javascript source

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,19 @@
+/*console.log('Hello World');
+document.write('Hello World');*/
+// This is a single line comment
+/*
+This 
+is a multiple 
+line 
+comment
+*/
+
+const productName = 'Baked Bean';
+let price = 1.50;
+let quantity = 3;
+
+let inStock = true;
+let discountAmount = 0.20;
+
+console.log(productName);
+console.log(price);
